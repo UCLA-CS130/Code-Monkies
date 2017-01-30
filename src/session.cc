84339f3b3c94ef4defa082318ae6c6617a566cf9
Joe Burton <jevbburton@gmail.com>
2017-01-30 22:09:44 +0000

    Refactor .h code into matching .cc files where appropriate

--- src/session.cc
+++ src/session.cc
@@ -4,3 +4,55 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include "session.h"
+
+void Session::do_read()
+{
+  auto self(shared_from_this());
+  socket_.async_read_some(boost::asio::buffer(data_, max_length),
+      [this, self](boost::system::error_code ec, std::size_t length)
+      {
+      if (!ec)
+      {
+      debugf("do_read got message of length %lu\n", length);
+      do_write(length);
+      }
+      });
+}
+
+void Session::do_write(std::size_t length)
+{
+  auto self(shared_from_this());
+  char *response = (char*) malloc(max_length);
+  if (response == NULL) {
+    debugf("do_write failed to allocate response buffer.\n");
+    return;
+  }
+
+  // Apparently boost doesn't care to null terminate its strings, so we
+  // have to do it ourselves.
+  if (length < max_length) {
+    data_[length] = 0;
+  } else {
+    // TODO: handle this
+    data_[max_length] = 0;
+  }
+
+  int response_len = snprintf(response, max_length, RESPONSE,
+      length, "text/plain", data_);
+
+  debugf("do_write sending response of length %d\n", response_len);
+
+  if (response_len >= max_length) {
+    // Output was truncated. TODO: handle this
+    debugf("do_write had to truncate response.\n");
+  }
+  boost::asio::async_write(socket_, boost::asio::buffer(response,
+        response_len),
+      [this, self](boost::system::error_code ec, std::size_t /*length*/)
+      {
+      if (!ec)
+      {
+      do_read();
+      }
+      });
+}
--- src/session.h
+++ src/session.h
@@ -2,8 +2,6 @@
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// TODO refactor most of this code into session.cc
 
 #ifndef SESSION_INCLUDED
 #define SESSION_INCLUDED
@@ -43,57 +41,12 @@ class Session
     /*
      * Read data from the client. On success, write back a response.
      */
-    void do_read()
-    {
-      auto self(shared_from_this());
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-          [this, self](boost::system::error_code ec, std::size_t length)
-          {
-          if (!ec)
-          {
-          debugf("do_read got message of length %lu\n", length);
-          do_write(length);
-          }
-          });
-    }
+    void do_read();
 
-    void do_write(std::size_t length)
-    {
-      auto self(shared_from_this());
-      char *response = (char*) malloc(max_length);
-      if (response == NULL) {
-        debugf("do_write failed to allocate response buffer.\n");
-        return;
-      }
-
-      // Apparently boost doesn't care to null terminate its strings, so we
-      // have to do it ourselves.
-      if (length < max_length) {
-        data_[length] = 0;
-      } else {
-        // TODO: handle this
-        data_[max_length] = 0;
-      }
-
-      int response_len = snprintf(response, max_length, RESPONSE,
-          length, "text/plain", data_);
-
-      debugf("do_write sending response of length %d\n", response_len);
-
-      if (response_len >= max_length) {
-        // Output was truncated. TODO: handle this
-        debugf("do_write had to truncate response.\n");
-      }
-      boost::asio::async_write(socket_, boost::asio::buffer(response,
-            response_len),
-          [this, self](boost::system::error_code ec, std::size_t /*length*/)
-          {
-          if (!ec)
-          {
-          do_read();
-          }
-          });
-    }
+    /*
+     * Write data to the client. On success, listen for more data.
+     */
+    void do_write(std::size_t length);
 
     tcp::socket socket_;
     enum { max_length = 65536 }; // TODO: why is this an enum?
