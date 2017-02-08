25bcd5e19f9e5bf11e0b89aecd6bc2cbe33de384
Joe Burton <jevbburton@gmail.com>
2017-02-08 22:20:11 +0000

    Refactor NginxConfig -> Config transform into ConfigBuilder method

--- src/config.cc
+++ src/config.cc
@@ -13,6 +13,89 @@ ConfigBuilder::ConfigBuilder()
   port = -1;
 }
 
+bool ConfigBuilder::build(const NginxConfig &config, Config *&conf)
+{
+  if (config.statements_.size() != 1) {
+    fprintf(stderr, "Incorrect number of top-level statements in server "
+        "configuration file. Got %lu, expected 1.\n",
+        config.statements_.size());
+    return false;
+  }
+
+  if (config.statements_[0]->child_block_ == NULL) {
+    fprintf(stderr, "Expected child block for top-level statement.\n");
+    return false;
+  }
+
+  for (auto const& statement :
+      config.statements_[0]->child_block_->statements_) {
+    size_t len = statement->tokens_.size();
+    if (len == 0) {
+      fprintf(stderr, "Invalid configuration file.\n");
+      return false;
+    } else if (len == 1) {
+      /*
+       * Must be a serve block or an echo block.
+       */
+      if (statement->tokens_[0] == "serve") {
+        for (auto const& tuple : statement->child_block_->statements_) {
+          if (tuple->tokens_.size() != 2) {
+            fprintf(stderr, "Invalid serve tuple: has length %lu, should have "
+                "length 2.\n", tuple->tokens_.size());
+            return false;
+          }
+          if (!addFileUriMapping(tuple->tokens_[0],
+                tuple->tokens_[1])) {
+            fprintf(stderr, "Invalid serve tuple.\n");
+            return false;
+          }
+        }
+      } else if (statement->tokens_[0] == "echo") {
+        for (auto const& uri : statement->child_block_->statements_) {
+          if (uri->tokens_.size() != 1) {
+            fprintf(stderr, "Invalid echo URI: received %lu tokens, should "
+                "have received 1.\n", uri->tokens_.size());
+            return false;
+          }
+          if (!addEchoUri(uri->tokens_[0])) {
+            fprintf(stderr, "Invalid serve tuple.\n");
+            return false;
+          }
+        }
+      } else {
+        fprintf(stderr, "Unexpected inner block: %s.\n",
+            statement->tokens_[0].c_str());
+        return false;
+      }
+    } else if (len == 2) {
+      /*
+       * Must be a port number
+       */
+      if (statement->tokens_[0] == "listen") {
+        if (!setPort(std::stoi(statement->tokens_[1]))) {
+          fprintf(stderr, "Received invalid port.\n");
+          return false;
+        }
+      } else {
+        fprintf(stderr, "Unexpected inner statement: %s.\n",
+            statement->ToString(0).c_str());
+        return false;
+      }
+    } else {
+      fprintf(stderr, "Unexpected inner statement: %s.\n",
+          statement->ToString(0).c_str());
+      return false;
+    }
+  }
+
+  if (!build(conf)) {
+    fprintf(stderr, "Failed to build valid server configuration.\n");
+    return false;
+  }
+
+  return true;
+}
+
 bool ConfigBuilder::setPort(int port)
 {
   if (port < 1 || port > PORT_MAX) {
