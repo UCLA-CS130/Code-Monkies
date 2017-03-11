23f9a5a9589129fc2dfd7bf1daab0ff6574b7783
Joe Burton <jevbburton@gmail.com>
2017-03-10 22:16:39 -0800

    Continue work on deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,11 +26,27 @@ else
     echo "${cwd: -9}"
 fi
 
+# Make sure directory is empty to minimize transfer time.
+make clean
+
 if [ -e "$archive_file" ]; then
     rm "$archive_file"
 fi
 
+# need this to go into archive after un-tar-ing
+cwd="${PWD##*/}"
+
 tar czvf "$archive_file" .
 
-echo "scp -i $ssh_key_file $archive_file ${ssh_user}@${server_ip}"
-scp -i "$ssh_key_file" "$archive_file" "${ssh_user}@${server_ip}"
+# Copy this directory over to server
+echo "scp -i $ssh_key_file $archive_file ${ssh_user}@${server_ip}:$archive_file"
+#scp -oStrictHostKeyChecking=no -i "$ssh_key_file" "$archive_file" \
+  "${ssh_user}@$server_ip:$archive_file"
+
+remote_cmd=`echo "cd /tmp; tar zxvf $archive_file; cd $cwd;"`
+remote_cmd=`echo "$remote_cmd docker build -t httpserver .;"`
+remote_cmd=`echo "$remote_cmd docker run --rm -t -p 8080:8080 httpserver;"`
+
+echo "$remote_cmd"
+
+ssh -oStrictHostKeyChecking=no -i "$ssh_key_file" "${ssh_user}@${server_ip}" "$remote_cmd"
