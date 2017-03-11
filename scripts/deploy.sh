2815e53cf9a44ab042d4f5eb21dfa50aab3b625c
Joe Burton <jevbburton@gmail.com>
2017-03-10 22:52:36 -0800

    Further progress

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Note that this utility must be run from the top level of the "
+echo "Code-Monkies repository, as such: ./scripts/deploy.sh"
+echo "If not called from here, it will not work."
+
 if [ $# -ne 3 ]; then
     echo "Usage: $0 server_ip ssh_user ssh_key_file"
     exit 1
@@ -9,43 +13,28 @@ server_ip="$1"
 ssh_user="$2"
 ssh_key_file="$3"
 
-cwd="$(pwd)"
-
-archive_file="/tmp/archive.tgz"
-
-# Navigate to top level directory.
-# TODO this isn't really a legit of locating our position in a hfs but it's
-# good enough for most sane use cases.
-if [ ${cwd: -5} == "scripts" ]; then
-    cd ..
-elif [ ${cwd: -9} != "webserver" ]; then
-    echo "This command must be run from the top level of the webserver repository."
-    exit 1
-else
-    echo "${cwd: -5}"
-    echo "${cwd: -9}"
-fi
+archive_file="archive.tgz"
 
 # Make sure directory is empty to minimize transfer time.
 make clean
 
 if [ -e "$archive_file" ]; then
-    rm "$archive_file"
+  echo "removing old archive file"
+  rm "$archive_file"
 fi
 
 # need this to go into archive after un-tar-ing
 cwd="${PWD##*/}"
 
-tar czvf "$archive_file" .
+tar czvf "$archive_file" "../$cwd"
 
 # Copy this directory over to server
-echo "scp -i $ssh_key_file $archive_file ${ssh_user}@${server_ip}:$archive_file"
-#scp -oStrictHostKeyChecking=no -i "$ssh_key_file" "$archive_file" \
-  "${ssh_user}@$server_ip:$archive_file"
+echo "scp -i $ssh_key_file $archive_file ${ssh_user}@${server_ip}:/tmp/$archive_file"
+scp -oStrictHostKeyChecking=no -i "$ssh_key_file" "$archive_file" \
+  "${ssh_user}@$server_ip:/tmp/$archive_file"
 
-remote_cmd=`echo "cd /tmp; tar zxvf $archive_file; cd $cwd;"`
-remote_cmd=`echo "$remote_cmd docker build -t httpserver .;"`
-remote_cmd=`echo "$remote_cmd docker run --rm -t -p 8080:8080 httpserver;"`
+remote_cmd=`echo "cd /tmp; rm -rf $cwd; tar zxvf $archive_file;"`
+remote_cmd=`echo "$remote_cmd  cd $cwd; sudo ./scripts/install_and_run.sh"`
 
 echo "$remote_cmd"
 
